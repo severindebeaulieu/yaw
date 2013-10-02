316e724d70636eefe4183f1e8dad3610737ae55e
unknown <sdebeaulieu@.omnilog.dom>
2013-10-02 10:12:59 +0200

    Init prestashop

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'JgNY16hqVG6YpIF5vbjvtvpDaVFT8CnWIFxyaCNBI52HAGvqmAYNaawF');
+define('_COOKIE_IV_', 'JvE5BqjC');
+define('_PS_CREATION_DATE_', '2013-10-02');
+define('_PS_VERSION_', '1.5.5.0');
+define('_RIJNDAEL_KEY_', 'u0y6r3Wu6WFfo6HdrOBWS8pOGA95pdrM');
+define('_RIJNDAEL_IV_', 'fmNnkYWJ5wD4YrgWArfDWw==');
